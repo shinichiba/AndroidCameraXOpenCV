b3f09fb5dfb01a01cf46b9b24f722869b4b57443
budinhba5@gmail.com <budinhba5@gmail.com>
2021-06-02 15:09:55 +0700

    Hello

---
+++ app/src/main/java/com/journaldev/androidcameraxopencv/MainActivity.java
@@ -0,0 +1,456 @@
+package com.journaldev.androidcameraxopencv;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageAnalysisConfig;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.Color;
+import android.graphics.Path;
+import android.graphics.Canvas;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+import android.util.Rational;
+import android.util.Size;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Core;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int x_center;
+    int y_center;
+    int points;
+
+    final private int REQUEST_CODE_PERMISSIONS = 101;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+    TextureView textureView;
+    ImageView ivBitmap;
+    LinearLayout llBottom;
+
+    int currentImageType = Imgproc.COLOR_RGB2GRAY;
+
+    ImageCapture imageCapture;
+    ImageAnalysis imageAnalysis;
+    Preview preview;
+
+    FloatingActionButton btnCapture, btnOk, btnCancel;
+
+    static {
+        if (!OpenCVLoader.initDebug())
+            Log.d("ERROR", "Unable to load OpenCV");
+        else
+            Log.d("SUCCESS", "OpenCV loaded");
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnCapture = findViewById(R.id.btnCapture);
+        btnOk = findViewById(R.id.btnAccept);
+        btnCancel = findViewById(R.id.btnReject);
+
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+
+        llBottom = findViewById(R.id.llBottom);
+        textureView = findViewById(R.id.textureView);
+        ivBitmap = findViewById(R.id.ivBitmap);
+
+        if (allPermissionsGranted()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+    }
+
+    private void startCamera() {
+
+        CameraX.unbindAll();
+        preview = setPreview();
+        imageCapture = setImageCapture();
+        imageAnalysis = setImageAnalysis();
+
+        //bind to lifecycle:
+        CameraX.bindToLifecycle(this, preview, imageCapture, imageAnalysis);
+    }
+
+
+    private Preview setPreview() {
+
+        Rational aspectRatio = new Rational(textureView.getWidth(), textureView.getHeight());
+        Size screen = new Size(textureView.getWidth(), textureView.getHeight()); //size of the screen
+
+
+        PreviewConfig pConfig = new PreviewConfig.Builder().setTargetAspectRatio(aspectRatio).setTargetResolution(screen).build();
+        Preview preview = new Preview(pConfig);
+
+        preview.setOnPreviewOutputUpdateListener(
+                new Preview.OnPreviewOutputUpdateListener() {
+                    @Override
+                    public void onUpdated(Preview.PreviewOutput output) {
+                        ViewGroup parent = (ViewGroup) textureView.getParent();
+                        parent.removeView(textureView);
+                        parent.addView(textureView, 0);
+
+                        textureView.setSurfaceTexture(output.getSurfaceTexture());
+                        updateTransform();
+                    }
+                });
+
+        return preview;
+    }
+
+
+    private ImageCapture setImageCapture() {
+        ImageCaptureConfig imageCaptureConfig = new ImageCaptureConfig.Builder().setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+                .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation()).build();
+        final ImageCapture imgCapture = new ImageCapture(imageCaptureConfig);
+
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                imgCapture.takePicture(new ImageCapture.OnImageCapturedListener() {
+                    @Override
+                    public void onCaptureSuccess(ImageProxy image, int rotationDegrees) {
+                        Bitmap bitmap = textureView.getBitmap();
+                        showAcceptedRejectedButton(true);
+                        ivBitmap.setImageBitmap(bitmap);
+                    }
+
+                    @Override
+                    public void onError(ImageCapture.UseCaseError useCaseError, String message, @Nullable Throwable cause) {
+                        super.onError(useCaseError, message, cause);
+                    }
+                });
+
+
+                /*File file = new File(
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "" + System.currentTimeMillis() + "_JDCameraX.jpg");
+                imgCapture.takePicture(file, new ImageCapture.OnImageSavedListener() {
+                    @Override
+                    public void onImageSaved(@NonNull File file) {
+                        Bitmap bitmap = textureView.getBitmap();
+                        showAcceptedRejectedButton(true);
+                        ivBitmap.setImageBitmap(bitmap);
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCapture.UseCaseError useCaseError, @NonNull String message, @Nullable Throwable cause) {
+
+                    }
+                });*/
+            }
+        });
+
+        return imgCapture;
+    }
+
+
+    private ImageAnalysis setImageAnalysis() {
+
+        // Setup image analysis pipeline that computes average pixel luminance
+        HandlerThread analyzerThread = new HandlerThread("OpenCVAnalysis");
+        analyzerThread.start();
+
+
+        ImageAnalysisConfig imageAnalysisConfig = new ImageAnalysisConfig.Builder()
+                .setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+                .setCallbackHandler(new Handler(analyzerThread.getLooper()))
+                .setImageQueueDepth(1).build();
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis(imageAnalysisConfig);
+
+        imageAnalysis.setAnalyzer(
+                (image, rotationDegrees) -> {
+                    //Analyzing live camera feed begins.
+
+                    Bitmap bitmap = textureView.getBitmap();
+
+
+                    if (bitmap == null)
+                        return;
+                    //bitmap = generateCircularBitmap(bitmap);
+                    // Calculate mid red point
+                    int wbitmap = bitmap.getWidth();
+                    int hbitmap = bitmap.getHeight();
+                    int x = 0;
+                    int y = 0;
+                    int all_x = 0;
+                    int all_y = 0;
+                    while(x < wbitmap) {
+                        while(y < hbitmap){
+                            int pixel = bitmap.getPixel(x, y);
+                            int redValue = Color.red(pixel);
+                            int blueValue = Color.blue(pixel);
+                            int greenValue = Color.green(pixel);
+                            if(redValue > 200 && blueValue < 70 && greenValue < 70){
+                                points++;
+                                all_x = all_x + x;
+                                all_y = all_y + y;
+                            }
+                            y++;
+                        }
+                        x++;
+                        y = 0;
+                    }
+                    y = 0;
+                    x = 0;
+                    if(points > 200) {
+                        x_center = all_x / points;
+                        y_center = all_y / points;
+                        points =0;
+                    }
+                    // End calculation
+                    Mat mat = new Mat();
+                    Utils.bitmapToMat(bitmap, mat);
+
+
+                   // Imgproc.cvtColor(mat, mat, currentImageType );
+                    Imgproc.circle (
+                            mat,                 //Matrix obj of the image
+                            new Point(x_center, y_center),    //Center of the circle
+                            20,                    //Radius
+                            new Scalar(255,255, 0),  //Scalar object for color
+                            4                     //Thickness of the circle
+                    );
+                    Imgproc.putText(mat, "Test:W"+wbitmap+":H"+hbitmap+":P"+ points,new Point(50, 50),Core.FONT_HERSHEY_SIMPLEX,0.5,new Scalar(0, 0, 0),2 );
+                   // Imgproc.cvtColor(mat, mat, currentImageType);
+                    Utils.matToBitmap(mat, bitmap);
+                    final Bitmap bitmap2 = bitmap;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ivBitmap.setImageBitmap(bitmap2);
+                        }
+                    });
+
+                });
+
+
+        return imageAnalysis;
+
+    }
+    public Bitmap generateCircularBitmap(Bitmap input) {
+
+        final int width = input.getWidth();
+        final int height = input.getHeight();
+        final Bitmap outputBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        final Path path = new Path();
+        // Calculate mid red point
+        int wbitmap = input.getWidth();
+        int hbitmap = input.getHeight();
+        int x = 0;
+        int y = 0;
+        int all_x = 0;
+        int all_y = 0;
+        while(x < wbitmap) {
+            while(y < hbitmap){
+                int pixel = input.getPixel(x, y);
+                int redValue = Color.red(pixel);
+                int blueValue = Color.blue(pixel);
+                int greenValue = Color.green(pixel);
+                if(redValue > 200 && blueValue < 70 && greenValue < 70){
+                    points++;
+                    all_x = all_x + x;
+                    all_y = all_y + y;
+                }
+                y++;
+            }
+            x++;
+            y = 0;
+        }
+        y = 0;
+        x = 0;
+        if(points > 200) {
+            x_center = all_x / points;
+            y_center = all_y / points;
+            points =0;
+        }
+        // End calculation
+
+        path.addCircle(x_center, y_center
+                , (float) Math.min(wbitmap/10, (hbitmap / 20))
+                , Path.Direction.CCW);
+
+        final Canvas canvas = new Canvas(outputBitmap);
+        canvas.clipPath(path);
+        canvas.drawBitmap(input, 0, 0, null);
+        return outputBitmap;
+    }
+    private void showAcceptedRejectedButton(boolean acceptedRejected) {
+        if (acceptedRejected) {
+            CameraX.unbind(preview, imageAnalysis);
+            llBottom.setVisibility(View.VISIBLE);
+            btnCapture.hide();
+            textureView.setVisibility(View.GONE);
+        } else {
+            btnCapture.show();
+            llBottom.setVisibility(View.GONE);
+            textureView.setVisibility(View.VISIBLE);
+            textureView.post(new Runnable() {
+                @Override
+                public void run() {
+                    startCamera();
+                }
+            });
+        }
+    }
+
+
+    private void updateTransform() {
+        Matrix mx = new Matrix();
+        float w = textureView.getMeasuredWidth();
+        float h = textureView.getMeasuredHeight();
+
+        float cX = w / 2f;
+        float cY = h / 2f;
+
+        int rotationDgr;
+        int rotation = (int) textureView.getRotation();
+
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                rotationDgr = 0;
+                break;
+            case Surface.ROTATION_90:
+                rotationDgr = 90;
+                break;
+            case Surface.ROTATION_180:
+                rotationDgr = 180;
+                break;
+            case Surface.ROTATION_270:
+                rotationDgr = 270;
+                break;
+            default:
+                return;
+        }
+
+        mx.postRotate((float) rotationDgr, cX, cY);
+        textureView.setTransform(mx);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.black_white:
+                currentImageType = Imgproc.COLOR_RGB2GRAY;
+                startCamera();
+                return true;
+
+            case R.id.hsv:
+                currentImageType = Imgproc.COLOR_RGB2HSV;
+                startCamera();
+                return true;
+
+            case R.id.lab:
+                currentImageType = Imgproc.COLOR_RGB2Lab;
+                startCamera();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnReject:
+                showAcceptedRejectedButton(false);
+                break;
+
+            case R.id.btnAccept:
+                File file = new File(
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "" + System.currentTimeMillis() + "_JDCameraX.jpg");
+                imageCapture.takePicture(file, new ImageCapture.OnImageSavedListener() {
+                    @Override
+                    public void onImageSaved(@NonNull File file) {
+                        showAcceptedRejectedButton(false);
+
+                        Toast.makeText(getApplicationContext(), "Image saved successfully in Pictures Folder", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCapture.UseCaseError useCaseError, @NonNull String message, @Nullable Throwable cause) {
+
+                    }
+                });
+                break;
+        }
+    }
+}
